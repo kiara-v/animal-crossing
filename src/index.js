0769e1516d21fd261565698d281d7b64914c6692
kiara-v <michelle_vong@brown.edu>
2023-10-26 22:59:11 -0400

    deploy

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-	{/* <h1>Click or press/hold space! (r to restart, move camera with the mouse)</h1> */}
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+// ReactDOM.createRoot(rootNode).render(<App />);
